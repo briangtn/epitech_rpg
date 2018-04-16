3a7b1860a717b37a78ca164a03caef19a9daa927
brian <brian.guitteny@epitech.eu>
2018-04-16 13:59:05 +0200

    Adding fight, attack and enemy structure (For the fighting system)

--- include/fight.h
+++ include/fight.h
@@ -10,4 +10,21 @@
 #ifndef __FIGHT__H_
 	#define __FIGHT__H_
 
+	typedef struct attack {
+		char *name;
+		float damage;
+		float power;
+	} attack_t;
+
+	typedef struct my_fight_enemy {
+		float life;
+		char *sprite_path;
+	} fight_enemy_t;
+
+	typedef struct my_fight {
+		char *background_path;
+		char *player_sprite_path;
+		float player_life;
+	} fight_t;
+
 #endif /* !__FIGHT__H_ */
