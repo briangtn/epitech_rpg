974ad0cef7e89c8e7887dc806bb0f83591cd96c5
Frabel Julian <julian.frabel@outlook.fr>
2018-04-19 14:18:53 +0200

    Added interaction component

--- include/prefabs.h
+++ include/prefabs.h
@@ -30,4 +30,26 @@
 
 	#pragma endregion /* !Player */
 
+	#pragma region CustomComponent
+
+	enum custom_component_s {
+		INTERACTION
+	};
+
+	#pragma region Interaction
+
+	typedef struct interaction_s {
+		void (*destroy)();
+		gameobject_t *parent;
+		void (*interaction)(gameobject_t *go, sf_engine_t *engine);
+		sf_engine_t *engine;
+	} sf_interaction_t;
+
+	sf_interaction_t *create_interaction(gameobject_t *parent);
+	void destroy_interaction(sf_interaction_t *interaction);
+
+	#pragma endregion
+
+	#pragma endregion
+
 #endif /* !__PREFABS__H_ */
