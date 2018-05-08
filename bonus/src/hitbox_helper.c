e36eb775f4a49893f99edc91458cc056d040b687
Frabel Julian <julian.frabel@outlook.fr>
2018-05-08 14:43:21 +0200

    Added collisions to castle and reduce fps to 30

--- bonus/src/hitbox_helper.c
+++ bonus/src/hitbox_helper.c
@@ -13,6 +13,8 @@ int main(int ac, char **av)
 {
 	int x1 = 0;
 	int y1 = 0;
+	int startx = 0;
+	int starty = 0;
 	int x2 = 0;
 	int y2 = 0;
 
@@ -20,13 +22,14 @@ int main(int ac, char **av)
 		dprintf(2, "./hitbox_helper x1 y1 x2 y2\n");
 		return (84);
 	}
+	x1 = atoi(av[1]);
+	y1 = atoi(av[2]);
 	x2 = atoi(av[3]);
 	y2 = atoi(av[4]);
-	for (x1 = atoi(av[1]); x1 <= x2; x1++) {
-		for (y1 = atoi(av[2]); y1 <= y2; y1++) {
-			printf("TILE_ID : \"1\" ; TILE_POS : \"%i\" x \"%i\" ; \
-EVENT : \"0\" ; TILE_TYPE : \"2\" ; LAYER : \"1\"\n", x1, y1);
-		}
-	}
+	for (startx = x1; startx <= x2; startx++)
+		for (starty = y1; starty <= y2; starty++)
+			printf((startx == x1 || startx == x2 || starty == y1 ||\
+starty == y2) ? "TILE_ID : \"1\" ; TILE_POS : \"%i\" x \"%i\" ; \
+EVENT : \"0\" ; TILE_TYPE : \"2\" ; LAYER : \"1\"\n" : "", startx, starty);
 	return (0);
 }
