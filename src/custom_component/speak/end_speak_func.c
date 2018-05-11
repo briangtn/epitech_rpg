bed08c2ad320e845db2bc74f77c0483f940a20f0
Frabel Julian <julian.frabel@outlook.fr>
2018-05-11 19:12:13 +0200

    Modified guard speach

--- src/custom_component/speak/end_speak_func.c
+++ src/custom_component/speak/end_speak_func.c
@@ -13,9 +13,19 @@ int give_sword(sf_engine_t *engine, UNUSED gameobject_t *parent)
 {
 	gameobject_t *player = engine->get_gameobject(engine, "player");
 	sf_inventory_t *inv = get_component(player, INVENTORY);
+	sf_speak_t *guard = create_speak_component(NULL);
 
+	if (guard == NULL)
+		return (0);
 	if (inv && inv->retrieve_item(inv, &(ITEM_LIST[ITEM_SWORD])) == -1) {
 		inv->add_item(inv, &(ITEM_LIST[ITEM_SWORD]));
+		guard->set_info(guard, "Here take my sword", FACE_SOLDIER);
+		guard->set_font(guard, FONT_SPEAK);
+		guard->show(guard, engine);
+	} else {
+		guard->set_info(guard, "Time is counted", FACE_SOLDIER);
+		guard->set_font(guard, FONT_SPEAK);
+		guard->show(guard, engine);
 	}
 	return (0);
 }
