7a7328b170b9efc7ea4e084bd733fe0c7f28674c
Frabel Julian <julian.frabel@outlook.fr>
2018-05-12 17:13:40 +0200

    Fixed a segfault on empty npc dialog line

--- src/prefabs/npc/npc_interaction.c
+++ src/prefabs/npc/npc_interaction.c
@@ -16,7 +16,8 @@ void npc_interaction(gameobject_t *npc, sf_engine_t *engine)
 {
 	sf_npc_t *npc_comp = get_component(npc, NPC);
 
-	if (npc_comp == NULL || npc_comp->speak == NULL)
+	if (npc_comp == NULL || npc_comp->speak == NULL || \
+npc_comp->speak->text == NULL || npc_comp->speak->text[0] == '\0')
 		return;
 	npc_comp->speak->show(npc_comp->speak, engine);
 }
