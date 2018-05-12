543d50ebf42df91325c994eed64ea33f3d4f9eee
Frabel Julian <julian.frabel@outlook.fr>
2018-05-12 11:59:23 +0200

    Added the damage display as a float

--- src/scenes/fight/fight_messages.c
+++ src/scenes/fight/fight_messages.c
@@ -9,11 +9,30 @@
 #include "my_sfml.h"
 #include "rpg.h"
 
+char *my_float_to_str(float nb)
+{
+	int nb_int = (int)nb;
+	int after_point = (int)((nb - nb_int) * 100.0);
+	char *int_nb = my_int_to_str(nb_int);
+	char *float_nb = malloc(sizeof(*float_nb) * (my_strlen(int_nb) + 4));
+	int i = 0;
+
+	if (float_nb == NULL)
+		return (int_nb);
+	for (i = 0; i < my_strlen(int_nb); i++)
+		float_nb[i] = int_nb[i];
+	float_nb[i++] = '.';
+	float_nb[i++] = after_point / 10 + '0';
+	float_nb[i++] = after_point % 10 + '0';
+	float_nb[i++] = '\0';
+	return (float_nb);
+}
+
 void ennemy_attack_message(fight_t *fight, fight_enemy_t *enemy)
 {
 	char *res = my_strdup("You took ");
 
-	my_strdupcat(&res, my_int_to_str(fight->last_attack->damage));
+	my_strdupcat(&res, my_float_to_str(fight->last_attack->damage));
 	if (fight->last_attack->damage == 1)
 		my_strdupcat(&res, " damage from ");
 	else
@@ -26,7 +45,7 @@ void player_attack_message(fight_t *fight, fight_enemy_t *enemy)
 {
 	char *res = my_strdup("You did ");
 
-	my_strdupcat(&res, my_int_to_str(fight->last_attack->damage));
+	my_strdupcat(&res, my_float_to_str(fight->last_attack->damage));
 	if (fight->last_attack->damage == 1)
 		my_strdupcat(&res, " damage from ");
 	else
